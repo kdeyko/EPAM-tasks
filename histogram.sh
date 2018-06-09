a6db0185480adcfb200dabc9c1d52e2f815d4efa
Kirill Deyko <kdeyko@gmail.com>
2018-06-09 18:41:09 +0300

    histogram on SED done!

--- histogram.sh
+++ histogram.sh
@@ -8,46 +8,20 @@ dir="workdir_for_epam_sed_task"
 mkdir $dir
 mv $file $dir
 
-### collecting temp files
-
-
-
-
-### working with range 0 - 9
+### working with range 0-9
 sed -n "s/^[0-9]$/\*/p" $dir/$file > $dir/temp
-sed -n '{
-$ i\
- 0 -  9: 
-$ =
-$ i \
- 
-$ r $dir/temp
-}' $dir/temp | sed ':a;N;$!ba;s/\n//g'
+sed -n -e "1 s/./ 0 -  9: /p" -e "$=" -e "$ a \ " -e "$ r $dir/temp" $dir/temp | sed ':a;N;$!ba;s/\n//g'
 
-
-### working with range 10 - 19
-sed -n "s/^1[0-9]$/\*/p" $dir/$file > $dir/temp
-sed -n '{
-$ i\
-10 - 19: 
-$ =
-$ i \
- 
-$ r $dir/temp
-}' $dir/temp | sed ':a;N;$!ba;s/\n//g'
-
+### working with ranges from 10 to 99
+for N in {1..9}
+do
+sed -n "s/^$N[0-9]$/\*/p" $dir/$file > $dir/temp
+sed -n -e "1 s/./"$N"0 - "$N"9: /p" -e "$=" -e "$ a \ " -e "$ r $dir/temp" $dir/temp | sed ':a;N;$!ba;s/\n//g'
+done
 
 ### last: counting "100" matches
 sed -n "s/100/\*/p" $dir/$file > $dir/temp
-sed -n '{
-$ i\
-    100: 
-$ =
-$ i \
- 
-$ r $dir/temp
-}' $dir/temp | sed ':a;N;$!ba;s/\n//g'
-
+sed -n -e "1 s/./    100: /p" -e "1 a \ " -e "$=" -e "$ a \ " -e "$ r $dir/temp" $dir/temp | sed ':a;N;$!ba;s/\n//g'
 
 ### cleaning...
-rm -rf $dir
+rm -r $dir
